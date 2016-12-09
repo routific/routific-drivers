c1b6e7b494810b4e6f31473206964bc875865f48
Philip Chung <philip@pmhc.co>
2016-12-08 16:24:27 -0800

    aggregate

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 angular.module('app', [])
 .component('app', {
-  controller: [function() {
-      var vm = this;
-      vm.test = 'a';
+  controller: ['dataService', function(dataService) {
+    
+    this.test = dataService.init();
+    
   }]
 })
-
 .component('driverList', {
   bindings: {
     data: '<'
   },
   template: `
-  <ul>
-    <li ng-repeat="driver in $ctrl.data track by $index">
-    <div class="driver-name">{{ driver.name }}</div>
-    <div class="driver-total-distance">1000m</div>
-    <div class="driver-avg-speed">100m/hr</div>
-    </li>
-  </ul>
+  <div class="list-title">
+    <div>Name</div>
+    <div>Total Distance</div>
+    <div>Total Time</div>
+    <div>Avg Speed</div>
+  </div>
+    <ul>
+      <li ng-repeat="driver in $ctrl.data track by $index">
+
+      <div class="list-row">
+        <div class="driver name">{{ driver.name }}</div>
+        <div class="driver total-distance">1000m</div>
+        <div class="driver total-time">100hr</div>
+        <div class="driver avg-speed">100m/hr</div>
+      </div>
+
+      <div class="list-details">
+
+
+
+      </div>
+      </li>
+    </ul>
   `,
   controller: [function() {
     var vm = this;
-    vm.data = [
-      { name: 'Bob' },
-      { name: 'Trevor' },
-      { name: 'Test' }
-    ]
+    
   }]
 
 })
--- dataService.js
+++ dataService.js
@@ -1,17 +1,69 @@
-angular.module('app', [])
+angular.module('app')
 .factory('dataService', ['$http', function($http) {
+
+ function getDrivers() {
+    return $http.get('data/drivers.json')
+      .then(function(res){
+        return res.data;
+      })
+  }
+  function getRoutes(date) {
+    return $http('data/' + date + '.json')
+      .then(function(res){
+        return res.data;
+      })
+  }
+
+  var _getDailyStats = function(route) {
+      var total = route.reduce(function(prev, cur) {
+         prev[totalTime] += cur[drivingTimeFromPrevious];
+         prev[totalDistance] += cur[distanceToPrevious];
+         return prev;
+      }, {
+          totalTime: 0,
+          totalDistance: 0
+      });
+
+      return total;
+  }
+
+  function aggregate() {
+    var response = {}
+    getDrivers()
+    .then(function(result) {
+      var aggreDriver = result.forEach(function(driverItem) {
+          response[driverItem.id] = driverItem;
+          response[driverItem.id]["daily"] = {};
+          response[driverItem.id]["totalTime"] = 0;
+          response[driverItem.id]["totalDistance"] = 0;
+      });
+      days = ['2016-12-05','2016-12-06']
+      routePromises = []
+      days.forEach(function(day) {
+        routePromises.push(getRoutes(day));
+      });
+      return Promise.all(routePromises);
+    })
+    .then(function(routeResults) {
+      routeResults.forEach(function(routesForDay) {
+        routesForDay = getRoutesForDay(day);
+        date = routesForDay.day;
+        routesForDay.routes.forEach(function(routeItem) {
+            driver = response[routeItem.driverId]
+            dailyStats = _getDailyStats(routeItem.route)
+            driver.daily[date] = dailyStats
+            driver.totalTime += dailyStats.totalTime
+            driver.totalDistance += dailyStats.totalDistance
+        });
+      });
+      console.log(response);
+      return response
+    })
+  }
+
   return {
-    getDrivers: function() {
-      return $http.get('data/drivers.json')
-        .then(function(res){
-          return res.data;
-        })
-    },
-    getRoutes: function(date) {
-      return $http('data/' + date + '.json')
-        .then(function(res){
-          return res.data;
-        })
+    init: function() {
+      return aggregate();
     }
   }
 }])
